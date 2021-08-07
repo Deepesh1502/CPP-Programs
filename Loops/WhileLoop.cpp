52414a9aa819d6414fe4045cdd0222c89ec916a2
Deepesh Garg <63232108+Deepesh1502@users.noreply.github.com>
2021-08-07 11:46:41 +0530

    Create WhileLoop.cpp

---
+++ Loops/WhileLoop.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a, i, n, o;
+  cout << "Select an option;
+  cout << "1) To Print numbers";
+  cout << "2) To print the tables";
+  cout << "Enter the option number: ";
+  cin >> o;
+
+/*The switch expression will
+determine the case 1 or 2 to be perform*/
+  switch (o)
+
+  {
+  case 1:
+    cout << "Enter the Initial: ";
+    cin >> i;
+    cout << "Enter the final: ";
+    cin >> n;
+    while (i <= n)
+    {
+      i++;
+      cout << i;
+    }
+
+    break;
+/*Break will exit the code and
+prevent to run case 2*/
+
+  case 2:
+    cout << "Enter the value for table of: ";
+    cin >> i;
+    a = 1;
+    while (n = i)
+    {
+      n <= (i * 10);
+      n = n + i;
+      a++;
+      cout << i << " * " << a << " = " << n;
+    }
+    break;
+  }
+}
