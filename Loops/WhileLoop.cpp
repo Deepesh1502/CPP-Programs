be0aac60b024e000a5a2a2af8bc9508926a3f5f0
Deepesh Garg <63232108+Deepesh1502@users.noreply.github.com>
2021-08-09 00:15:11 +0530

    Update WhileLoop.cpp
    Switch case has been removed

--- Loops/WhileLoop.cpp
+++ Loops/WhileLoop.cpp
@@ -2,44 +2,15 @@
 using namespace std;
 int main()
 {
-  int a, i, n, o;
-  cout << "Select an option\n";
-  cout << "1) To Print numbers\n";
-  cout << "2) To print the tables\n";
-  cout << "Enter the option number: ";
-  cin >> o;
-
-/*The switch expression will
-determine the case 1 or 2 to be perform*/
-  switch (o)
-
+  int i, n;
+  cout << "1) To Print numbers with While Loop\n";
+  cout << "Enter the Initial: ";
+  cin >> i;
+  cout << "Enter the final: ";
+  cin >> n;
+  while (i <= n)
   {
-  case 1:
-    cout << "Enter the Initial: ";
-    cin >> i;
-    cout << "Enter the final: ";
-    cin >> n;
-    while (i <= n)
-    {
-      i++;
-      cout << i;
-    }
-
-    break;
-/*Break will exit the code and
-prevent to run case 2*/
-
-  case 2:
-    cout << "Enter the value for table of: ";
-    cin >> i;
-    a = 1;
-    while (n = i)
-    {
-      n <= (i * 10);
-      n = n + i;
-      a++;
-      cout << i << " * " << a << " = " << n;
-    }
-    break;
+    i++;
+    cout << i << "\n";
   }
 }
