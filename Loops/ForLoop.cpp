e214c52fecca6ec4670a1ab93c89051b32bc96e6
Deepesh Garg <63232108+Deepesh1502@users.noreply.github.com>
2021-08-09 00:12:23 +0530

    Update ForLoop.cpp
    The switch case has been removed

--- Loops/ForLoop.cpp
+++ Loops/ForLoop.cpp
@@ -2,41 +2,14 @@
 using namespace std;
 int main()
 {
-  int a, i, n, o;
-  cout << "Select an option\n";
-  cout << "1) To get tables of a number\n";
-  cout << "2) To print numbers\n";
-  cout << "Enter the option number: ";
+  int i, n;
+  cout << "To print numbers\n";
+  cout << "Enter Initial value: ";
+  cin >> i;
+  cout << "Enter Final value: ";
   cin >> n;
-
-/*The switch expression will
-determine the case 1 or 2 to be perform*/
-  switch (o)
-
+  for (i; i <= n; i++)
   {
-    case 1:
-      cout << "Enter value for table of: ";
-      cin >> i;
-      a = 1;
-      for (n = i; n <= (i * 10); n += i)
-      {
-        cout << i << " * " << a << " = " << n;
-	a++;
-      }
-
-      break;
-/*Break will exit the code and
-prevent to run case 2*/
-
-    case 2:
-      cout << "Enter Initial value: ";
-      cin >> i;
-      cout << "Enter Final value: ";
-      cin >> n;
-      for (i; i <= n; i++)
-      {
-        cout << i;
-      }
-      break;
+    cout << i << "\n";
   }
 }
